f2e5d469d9b7f247280c8314bab6fb5a5476faaa
JoaquinFeola <joacoarielfeola@gmail.com>
2026-06-12 02:22:46 -0300

    fixed build errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import vitest from 'vitest/config';
+import 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
